8c7cf2708cf309bf5fe34354bbc876c5cfdef6a4
dels-in <chernenko3954@mail.ru>
2024-02-16 20:12:49 +0300

    создал модель для добавления нового отзыва

---
+++ Review.Domain/Models/AddReview.cs
@@ -0,0 +1,9 @@
+namespace ReviewsWebApplication.Models;
+
+public class AddReview
+{
+    public int ProductId { get; set; }
+    public int UserId { get; set; }
+    public string Text { get; set; }
+    public int Grade { get; set; }
+}
