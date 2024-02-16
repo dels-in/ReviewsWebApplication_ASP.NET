8898d6157d7ce90ab0751079fae3c3d0f3c23d26
dels-in <chernenko3954@mail.ru>
2024-02-16 20:12:59 +0300

    создал модель для маппинга с бд

---
+++ ReviewsWebApplication/Models/AddReviewViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReviewsWebApplication.Models;
+
+public class AddReviewViewModel
+{
+    public int ProductId { get; set; }
+    public int UserId { get; set; }
+    public string Text { get; set; }
+    public int Grade { get; set; }
+}
